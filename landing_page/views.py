ef3efb0f8a0e48665358d31af6baa9cfef08f899
JohnDeVries <jdevries3133@gmail.com>
2020-08-08 13:09:18 -0400

    Started home page

--- landing_page/views.py
+++ landing_page/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'resume_content.html')
+    return render(request, 'home.html')
--- resume_website/settings.py
+++ resume_website/settings.py
@@ -123,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'resume_website', 'static'),
+    os.path.join(BASE_DIR, 'landing_page', 'static'),
+]
