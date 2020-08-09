b733255b58724fc26c6355bbee8b17b27ebbfaa9
JohnDeVries <jdevries3133@gmail.com>
2020-08-09 08:32:47 -0400

    Home page is coming along

---
+++ landing_page/static/animation_helper.js
@@ -0,0 +1,13 @@
+function positionSurprise() {
+  const TOP_OFFSET = 41;
+  const LEFT_OFFSET = 19;
+  const welcome = document.getElementById("welcome");
+  const surprise = document.getElementById("surprise");
+  const rect = welcome.getBoundingClientRect();
+  const Top = Math.floor(rect.y) - TOP_OFFSET;
+  const left = Math.floor(rect.x) - LEFT_OFFSET;
+  surprise.setAttribute("style", `top: ${Top}px; left: ${left}px;`);
+}
+
+positionSurprise();
+window.onresize = positionSurprise;
